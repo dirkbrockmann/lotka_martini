71af9db4f118383dc8676a4054a9ee3da7b7485c
Dirk Brockmannn <brockmann.dirk@gmail.com>
2024-04-17 19:21:17 +0200

    zutte

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,6 @@
 export default {
-  "title": "Lotka Martini",
-  "subtitle":"The Lotka-Volterra model",
-  "description": "This explorable illustrates oscillatory dynamics and spatio-temporal patterns in the most famous predator prey model on a lattice."
+  "title": "Fuchs & Hase",
+  "subtitle": "Ein berühmtes Räuber-Beute Modell",
+  "description": "Das Explorable zeigt die Dynamik eines einfachen Räuber-Beute Modells, des sogenannten Lotka-Volterra Modells. Auf einem Gitter zeigt das Modell komplexe Muster und Oszillationen bei bestimmten Parameterwerten.",
+  
 }
